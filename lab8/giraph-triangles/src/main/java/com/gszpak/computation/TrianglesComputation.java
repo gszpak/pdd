c66d423286ee2b3756eee201fe60650052a60f32
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2015-04-23 00:26:23 +0200

    lab8 - triangles - second version

--- lab8/giraph-triangles/src/main/java/com/gszpak/computation/TrianglesComputation.java
+++ lab8/giraph-triangles/src/main/java/com/gszpak/computation/TrianglesComputation.java
@@ -8,14 +8,8 @@ import org.apache.giraph.graph.Vertex;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 
-import com.gszpak.App;
-
 public class TrianglesComputation extends
 		BasicComputation<IntWritable, IntWritable, NullWritable, IntWritable> {
-
-
-	private static final IntWritable ZERO_VALUE = new IntWritable(0);
-
 
 	private void propagateId(Vertex<IntWritable, IntWritable, NullWritable> vertex) {
 		for (Edge<IntWritable, NullWritable> outEdge : vertex.getEdges()) {
@@ -36,7 +30,7 @@ public class TrianglesComputation extends
 		}
 	}
 
-	private int countTriangles(Vertex<IntWritable, IntWritable, NullWritable> vertex,
+	private void countTriangles(Vertex<IntWritable, IntWritable, NullWritable> vertex,
 			Iterable<IntWritable> messages) {
 		int result = 0;
 		for (IntWritable possibleTriangleVertex: messages) {
@@ -44,59 +38,19 @@ public class TrianglesComputation extends
 				result++;
 			}
 		}
-		return result;
-	}
-
-	private int collectResult(Vertex<IntWritable, IntWritable, NullWritable> vertex,
-			Iterable<IntWritable> messages) {
-		int result = 0;
-		for (IntWritable incoming: messages) {
-			result += incoming.get();
-		}
-		return result;
-	}
-
-	private void propagateResult(Vertex<IntWritable, IntWritable, NullWritable> vertex,
-			Iterable<IntWritable> messages, int result) {
-		IntWritable minNeighbour = new IntWritable(Integer.MAX_VALUE);
-		for (Edge<IntWritable, NullWritable> outEdge: vertex.getEdges()) {
-			if (outEdge.getTargetVertexId().compareTo(minNeighbour) < 0) {
-				minNeighbour = outEdge.getTargetVertexId();
-			}
-		}
-		sendMessage(minNeighbour, new IntWritable(result));
-	}
-
-	private void saveResult(Vertex<IntWritable, IntWritable, NullWritable> vertex,
-			Iterable<IntWritable> messages, int result) {
-		assert vertex.getId().equals(App.MIN_VERTEX);
-		int actVal = vertex.getValue().equals(ZERO_VALUE) ? 0 : vertex.getValue().get();
-		actVal += result;
-		vertex.setValue(new IntWritable(actVal));
-	}
-
-	private void propagateOrSaveResult(Vertex<IntWritable, IntWritable, NullWritable> vertex,
-			Iterable<IntWritable> messages, int result) {
-		if (vertex.getId().equals(App.MIN_VERTEX)) {
-			saveResult(vertex, messages, result);
-		} else {
-			propagateResult(vertex, messages, result);
-		}
+		vertex.setValue(new IntWritable(result));
 	}
 
 	@Override
 	public void compute(Vertex<IntWritable, IntWritable, NullWritable> vertex,
 			Iterable<IntWritable> messages) throws IOException {
 		if (getSuperstep() == 0) {
+			vertex.setValue(new IntWritable(0));
 			propagateId(vertex);
 		} else if (getSuperstep() == 1) {
 			propagatePrevious(vertex, messages);
 		} else if (getSuperstep() == 2) {
-			int result = countTriangles(vertex, messages);
-			propagateOrSaveResult(vertex, messages, result);
-		} else {
-			int result = collectResult(vertex, messages);
-			propagateOrSaveResult(vertex, messages, result);
+			countTriangles(vertex, messages);
 		}
 		vertex.voteToHalt();
 	}
